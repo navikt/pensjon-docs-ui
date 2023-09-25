d6b5cf62e13fe6af5ee99dd4e74babb0108b4aaa
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2023-09-25 11:51:58 +0200

    Overwrite with local highlight.js config

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pensjon-docs-ui
 Antora UI (modifisert) for pensjon-docs-site
+
+## Modifikasjoner
+
+- highlight.js
+  - Legger til ymse keywords + literals for å støtte eksisterende pseudokode.
+    - Ruby er hovedsaklig angitt som source-language, da den gir best match i asciidoc-plugin preview (som ikke benytter highlight.js, men coderay)
+  - Fjerner en bråte språk som ikke er nødvendige
---
+++ src/js/vendor/highlight.bundle.js
@@ -0,0 +1,34 @@
+;(function () {
+  'use strict'
+
+  var hljs = require('highlight.js/lib/highlight')
+
+  // Customization: fjernet en mengde språk som ikke benyttes
+  // (evnt liten sannsynlighet for at kommer til å bli brukt)
+  hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
+  hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
+  hljs.registerLanguage('kotlin', require('highlight.js/lib/languages/kotlin'))
+  hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+  hljs.registerLanguage('none', require('highlight.js/lib/languages/plaintext'))
+  hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
+  hljs.registerLanguage('python', require('highlight.js/lib/languages/python'))
+  hljs.registerLanguage('ruby', require('highlight.js/lib/languages/ruby'))
+  hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
+  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+  hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+
+  // Customization: legger til diverse keywords runtime tilpasset pseudokoden i den gamle dok'en
+  // https://github.com/highlightjs/highlight.js/issues/1271
+  // Note: leading space
+  hljs.getLanguage('ruby').keywords += ' HVIS ELLER ELLERS IKKE SLUTT START END'
+  hljs.getLanguage('ruby').keywords += ' LOOP FOR SETT RETURN RETURNER'
+
+  // ...og literals
+  hljs.getLanguage('ruby').literals += ' TRUE true FALSE false SANN USANN NULL'
+
+  ;[].slice.call(document.querySelectorAll('pre code.hljs[data-lang]')).forEach(function (node) {
+      hljs.highlightBlock(node)
+  })
+})()
